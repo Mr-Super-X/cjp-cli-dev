482e77985537b9d2c385d598948d90de263d0d56
v_jpch <v_jpch.digitalgd.com>
2024-09-29 16:09:06 +0800

    chore: update

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -4,7 +4,7 @@ const importLocal = require("import-local");
 const log = require("npmlog");
 
 if (importLocal(__filename)) {
-  log.info("cli", "正在使用 cjp-cli 本地版本");
+  log.info("cli", "正在使用 cjp-cli-dev 本地版本");
 } else {
   require("../lib")(process.argv.slice(2));
 }
