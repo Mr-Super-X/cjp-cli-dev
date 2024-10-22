d5faa3d7d76eb2d1785f941e606240d4099f7f0f
v_jpch <v_jpch.digitalgd.com>
2024-10-22 13:27:56 +0800

    feat: 增加快速删除本地和远程分支命令delete-branch，优化其它命令提示

---
+++ core/cli/lib/execDeleteBranch.js
@@ -0,0 +1,121 @@
+// 第三方库
+const simpleGit = require("simple-git"); // 用于在node程序中运行git
+// 自建库
+const log = require("@cjp-cli-dev/log"); // 用于给log信息添加各种自定义风格
+const { prompt, CLI_NAME } = require("@cjp-cli-dev/utils"); // 工具方法
+
+// 将当前进程执行的上下文路径传给simpleGit
+const git = simpleGit(process.cwd());
+
+module.exports = async function (name, options, command) {
+  // 定义分支名称，接收命令后面直接传参和指定参数--branch传参
+  let branchName = "";
+  if (name) {
+    branchName = name;
+  } else {
+    branchName = options.branch;
+  }
+
+  // 找出所需要的参数
+  const commandOptions = command.options.map((item) => ({
+    flag: item.flags,
+    description: item.description,
+  }));
+
+  if (!branchName) {
+    log.warn(
+      `请指定您想删除的分支名称，支持以下参数：\n\n${commandOptions
+        .map((option) => `['${option.flag}'：${option.description}]`)
+        .join(
+          "\n"
+        )}\n\n您可以输入 ${CLI_NAME} ${command.name()} -h 查看使用帮助`
+    );
+    return;
+  }
+
+  // 获取force参数
+  const { force } = options;
+
+  // 支持force参数直接删除，无需二次确认
+  if (force) {
+    // 检查并删除本地分支
+    await checkLocalBranch(branchName);
+    // 检查并删除远程分支
+    await checkRemoteBranch(branchName);
+  } else {
+    // 二次确认
+    const confirmDelete = await getConfirmDelete(branchName);
+
+    if (confirmDelete) {
+      // 检查并删除本地分支
+      await checkLocalBranch(branchName);
+      // 检查并删除远程分支
+      await checkRemoteBranch(branchName);
+    } else {
+      log.notice(`您已取消删除分支：${branchName}`);
+    }
+  }
+};
+
+// 检查并删除本地分支
+async function checkLocalBranch(branchName) {
+  log.info("检查本地是否存在分支：" + branchName);
+  const localBranchList = await git.branchLocal();
+  const hasRollback = localBranchList.all.find((item) =>
+    item.includes(branchName)
+  );
+
+  if (!hasRollback) {
+    log.info(`本地分支 ${branchName} 不存在，跳过删除分支`);
+    return false;
+  } else {
+    log.info(`本地分支 ${branchName} 存在，自动删除该分支`);
+    await deleteLocalBranch(branchName);
+    return true;
+  }
+}
+
+// 检查并删除远程分支
+async function checkRemoteBranch(branchName) {
+  log.info("检查远程是否存在分支：" + branchName);
+  const remoteBranchList = await git.branch(["-r"]);
+
+  const hasRollback = remoteBranchList.all.find((item) =>
+    item.includes(branchName)
+  );
+
+  if (!hasRollback) {
+    log.info(`远程分支 ${branchName} 不存在，跳过删除分支`);
+    return false;
+  } else {
+    log.info(`远程分支 ${branchName} 存在，自动删除该分支`);
+    await deleteRemoteBranch(branchName);
+    return true;
+  }
+}
+
+// 删除本地开发分支
+async function deleteLocalBranch(branchName) {
+  log.info("开始删除本地分支", branchName);
+  await git.deleteLocalBranch(branchName);
+  log.success(`删除本地分支 ${branchName} 成功`);
+}
+
+// 删除远程开发分支
+async function deleteRemoteBranch(branchName) {
+  log.info("开始删除远程分支", branchName);
+  await git.push(["origin", "--delete", branchName]);
+  log.success(`删除远程分支 ${branchName} 成功`);
+}
+
+// 二次确认
+async function getConfirmDelete(branchName) {
+  const { confirmDelete } = await prompt({
+    type: "confirm", // type为confirm时，默认值是true，则提示的字母Y为大写，否则提示的字母N为大写
+    name: "confirmDelete",
+    default: false,
+    message: `您确定要同时删除本地和远程 ${branchName} 分支吗？`,
+  });
+
+  return confirmDelete;
+}
--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,7 +1,6 @@
 "use strict";
 
 // 内置库
-const fs = require("fs"); // 用于文件操作
 const os = require("os"); // 用于获取系统信息
 const path = require("path"); // 用于获取路径
 // 第三方库
@@ -15,15 +14,15 @@ const exec = require("@cjp-cli-dev/exec"); // 用于执行动态初始化命令
 const { getNpmSemverVersion } = require("@cjp-cli-dev/get-npm-info"); // 用于获取npm包信息
 const {
   pathExists,
-  prompt,
   semver,
-  fse,
   colors,
   CLI_NAME,
   DEFAULT_CLI_HOME,
-  DEPENDENCIES_CACHE_DIR,
 } = require("@cjp-cli-dev/utils"); // 工具方法
-const pkg = require("../package.json");
+const pkg = require("../package.json"); // 脚手架package.json
+// 简单命令
+const execClean = require("./execClean"); // 执行清除缓存命令
+const execDeleteBranch = require("./execDeleteBranch"); // 执行删除分支命令
 
 // 全局变量
 const homedir = os.homedir(); // 用户主目录
@@ -110,7 +109,14 @@ function registerCommander() {
       exec(...args); // 这种写法也可以
     });
 
-  // 发布回滚
+  // 添加复用代码
+  program
+    .command("add [templateName]")
+    .description("添加组件代码片段模板、页面标准模板、自定义页面模板到本地项目")
+    .option("-reg, --registry <registry>", "指定npm源地址", "")
+    .action(exec);
+
+  // 回滚master
   program
     .command("rollback")
     .description("快速回滚master分支代码")
@@ -118,12 +124,15 @@ function registerCommander() {
     .option("-bc, --buildCmd <buildCmd>", "指定自定义构建命令", "npm run build")
     .action(exec);
 
-  // 添加复用代码
+  // 快速删除本地和远程分支
   program
-    .command("add [templateName]")
-    .description("添加组件代码片段模板、页面标准模板、自定义页面模板到本地项目")
-    .option("-reg, --registry <registry>", "指定npm源地址", "")
-    .action(exec);
+    .command("delete-branch [branchName]")
+    .description("快速删除本地和远程分支")
+    .option("-b, --branch <branchName>", "指定分支名称", "")
+    .option("-f, --force", "是否强制删除分支", false)
+    .action((name, options, command) => {
+      execDeleteBranch(name, options, command);
+    });
 
   // 清除缓存
   program
@@ -132,42 +141,7 @@ function registerCommander() {
     .option("-a, --all", "清空全部缓存", false)
     .option("-d, --dep", "仅清空依赖缓存", false)
     .action((options, command) => {
-      const requireKeys = ["all", "dep"];
-
-      // 检查是否没传参数
-      function checkKeys(keys, obj) {
-        let result = false;
-
-        keys.forEach((key) => {
-          if (obj[key] === true) {
-            result = true;
-          }
-        });
-
-        return result;
-      }
-
-      // 找出所需要的参数
-      const commandOptions = command.options.map((item) => ({
-        flag: item.flags,
-        description: item.description,
-      }));
-
-      if (!checkKeys(requireKeys, options)) {
-        log.warn(
-          `请指定参数确认您想清除的内容，支持以下参数：\n\n${commandOptions
-            .map((option) => `['${option.flag}'：${option.description}]`)
-            .join(
-              "\n"
-            )}\n\n您可以输入 ${CLI_NAME} ${command.name()} -h 查看使用帮助`
-        );
-      }
-
-      if (options.all) {
-        cleanAll();
-      } else if (options.dep) {
-        cleanDep();
-      }
+      execClean(options, command);
     });
 
   // 高级功能：监听debug事件，开启debug模式
@@ -315,55 +289,4 @@ function checkRoot() {
 
 function checkCliVersion() {
   log.info("cli版本", pkg.version);
-}
-
-async function getConfirmClean(msg) {
-  // 二次确认
-  const { confirmClean } = await prompt({
-    type: "confirm",
-    name: "confirmClean",
-    default: false,
-    message: msg,
-  });
-
-  return confirmClean;
-}
-
-// 清空所有缓存
-async function cleanAll() {
-  if (!fs.existsSync(process.env.CLI_HOME_PATH)) {
-    log.warn("缓存路径不存在", process.env.CLI_HOME_PATH);
-    return;
-  }
-
-  const confirmClean = await getConfirmClean(
-    "确认要清除所有缓存吗？（注意：此操作将删除所有缓存数据）"
-  );
-
-  // 用户选择不确认，中断执行
-  if (!confirmClean) return;
-  log.info("开始清除所有缓存");
-  fse.emptyDirSync(process.env.CLI_HOME_PATH);
-  log.success("清除所有缓存成功", process.env.CLI_HOME_PATH);
-}
-
-// 清空依赖文件
-async function cleanDep() {
-  const depPath = path.resolve(
-    process.env.CLI_HOME_PATH,
-    DEPENDENCIES_CACHE_DIR
-  );
-  if (!fs.existsSync(depPath)) {
-    log.success("依赖缓存路径不存在", depPath);
-    return;
-  }
-  const confirmClean = await getConfirmClean(
-    "确认要清除依赖缓存吗？（注意：此操作将删除所有依赖缓存数据）"
-  );
-
-  // 用户选择不确认，中断执行
-  if (!confirmClean) return;
-  log.info("开始清除依赖缓存文件");
-  fse.emptyDirSync(depPath);
-  log.success("清除依赖缓存文件成功", depPath);
 }
