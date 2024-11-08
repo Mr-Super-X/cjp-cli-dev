91a9f8df2f9ab7fa6c35b65e1ad84f27c78a0f4e
v_jpch <v_jpch.digitalgd.com>
2024-11-08 12:34:34 +0800

    feat: server命令增加多服务器代理功能支持

--- commands/server/lib/index.js
+++ commands/server/lib/index.js
@@ -22,6 +22,8 @@ class ServerCommand extends Command {
     this.port = port;
     this.publicPath = null;
     this.proxyConfirm = null; // 是否需要代理
+    this.proxyMultipleConfirm = null; // 是否代理多个服务器
+    this.multipleProxy = null; // 代理多服务器配置
     this.origin = null; // 代理地址
     this.pathRewrite = null; // 重写代理地址
     // debug模式下输出以下变量
@@ -32,12 +34,20 @@ class ServerCommand extends Command {
     try {
       // 准备工作
       await this.prepare();
-      // 获取publicPath、origin、apiPrefix、pathRewrite等参数
+      // 获取publicPath
       await this.getPublicPath();
       await this.getProxyConfirm();
+      await this.getMultipleConfirm();
+      // 是否需要代理
       if (this.proxyConfirm) {
-        await this.getOrigin();
-        await this.getPathRewrite();
+        // 代理多个服务器
+        if (this.proxyMultipleConfirm) {
+          await this.getMultipleProxy();
+        } else {
+          // 代理单个服务器
+          await this.getOrigin();
+          await this.getPathRewrite();
+        }
       }
       // 启动服务
       await this.startExpress();
@@ -83,11 +93,19 @@ class ServerCommand extends Command {
     app.use(express.static(staticDirectory));
 
     if (this.proxyConfirm) {
-      // 单个代理
-      const proxy = await this.createSingleProxyMiddleware();
-
-      // 使用代理中间件处理所有请求（只会对匹配的请求进行代理）
-      app.use(proxy);
+      if (this.proxyMultipleConfirm) {
+        // 多个代理
+        const proxies = await this.createMultipleProxyMiddleware();
+        proxies.forEach((proxy) => {
+          // 使用中间件（区分路径来代理不同服务器）
+          app.use(proxy.apiPrefix, proxy.middleware);
+        });
+      } else {
+        // 单个代理
+        const proxy = await this.createSingleProxyMiddleware();
+        // 使用代理中间件处理所有请求（只会对匹配的请求进行代理）
+        app.use(proxy);
+      }
     }
 
     // 让所有路由都指向当前目录中的index.html
@@ -100,14 +118,51 @@ class ServerCommand extends Command {
 
     // 启动服务
     app.listen(port, ip, () => {
+      const publicPath = this.publicPath === "./" ? "/" : this.publicPath;
       log.success(
-        `本地预览服务器已启动，复制链接到浏览器地址栏进行访问\n\nhttp://${ip}:${port}${this.publicPath}/\n`
+        `本地预览服务启动成功，复制链接到浏览器地址栏进行访问\n\nhttp://${ip}:${port}${publicPath}\n`
       );
     });
   }
 
+  // 生成多个代理服务中间件配置
+  async createMultipleProxyMiddleware() {
+    log.info("开始生成多服务器代理中间件");
+
+    if (!this.multipleProxy) {
+      log.error("多服务器代理配置不存在，请重试");
+      process.exit(1);
+    }
+
+    const proxies = [];
+
+    const multipleProxy = JSON.parse(this.multipleProxy);
+
+    multipleProxy.forEach((item) => {
+      if (!item.apiPrefix && !item.target) {
+        log.error("必要属性 apiPrefix 或 target 不存在，请重试");
+        process.exit(1);
+      }
+      const defaultOptions = {
+        target: item.target, // 目标服务器地址
+        pathRewrite: item.pathRewrite || {}, // 重写请求路径
+        changeOrigin: true, // 支持跨域
+        ws: true, // 代理websocket请求
+        logLevel: "debug", // 日志级别
+      };
+
+      // 创建中间件
+      const proxy = createProxyMiddleware(defaultOptions);
+
+      proxies.push({ apiPrefix: item.apiPrefix, middleware: proxy });
+    });
+
+    log.success("多服务器代理中间件生成成功");
+    return proxies;
+  }
+
   async createSingleProxyMiddleware() {
-    log.info("开始生成代理服务配置");
+    log.info("开始生成单服务器代理中间件");
 
     if (!this.origin) {
       log.error("代理目标服务器地址不存在，请重试");
@@ -123,11 +178,18 @@ class ServerCommand extends Command {
     };
 
     if (this.pathRewrite) {
-      const keyArr = this.pathRewrite.split(" : ");
-      const apiPrefix = keyArr[0].trim();
-      const rewritePath = keyArr[1].trim();
+      const pathRewriteObj = JSON.parse(this.pathRewrite);
+      let apiPrefix;
+      let rewritePath;
+      // 读取前缀和匹配路径
+      const pathRewriteKeys = Object.keys(pathRewriteObj);
+      // 兼容处理，如果出现极端边界情况拿到了多个键值对，只取第一个
+      apiPrefix = pathRewriteKeys[0];
+      rewritePath = pathRewriteObj[pathRewriteKeys[0]];
+
       log.verbose("apiPrefix", apiPrefix);
       log.verbose("rewritePath", rewritePath);
+      // 添加到proxyOptions
       proxyOptions.pathRewrite = {
         [apiPrefix]: rewritePath,
       };
@@ -135,7 +197,7 @@ class ServerCommand extends Command {
 
     const proxy = createProxyMiddleware(proxyOptions);
 
-    log.success("代理服务配置生成成功");
+    log.success("单服务器代理中间件生成成功");
     return proxy;
   }
 
@@ -201,6 +263,58 @@ class ServerCommand extends Command {
     this.proxyConfirm = proxyConfirm;
   }
 
+  async getMultipleConfirm() {
+    const { proxyMultipleConfirm } = await prompt({
+      type: "confirm",
+      name: "proxyMultipleConfirm",
+      message: "是否有代理多个服务器需求？",
+      default: false, // 按回车默认为否
+    });
+
+    this.proxyMultipleConfirm = proxyMultipleConfirm;
+  }
+
+  async getMultipleProxy() {
+    const formatStr = `[
+  {
+    "apiPrefix": "/api1",
+    "target": "http://example1.com:8888",
+    "pathRewrite": {
+      "/api": ""
+    }
+  },
+  {
+    "apiPrefix": "/api2",
+    "target": "http://192.168.8.8:8888",
+    "pathRewrite": {
+      "/api": "/test"
+    }
+  }
+]`;
+    log.info(
+      `multipleProxy数据格式为JSON数组对象格式：\n\n ${formatStr} \n\n必传属性为：apiPrefix（匹配到该路径将代理到target服务器）、target（代理服务器地址） \n查看pathRewrite配置文档：https://github.com/chimurai/http-proxy-middleware?tab=readme-ov-file#pathrewrite-objectfunction\n`
+    );
+    const { multipleProxy } = await prompt({
+      type: "input",
+      name: "multipleProxy",
+      message: "请输入multipleProxy配置：",
+      default: "", // 默认 ''
+      validate(value) {
+        const done = this.async();
+        if (!value || !Array.isArray(JSON.parse(value))) {
+          done(
+            `输入的multipleProxy格式不合法，正确格式为JSON数组对象：\n\n${formatStr} \n\n必传属性：apiPrefix（匹配到该路径将代理到target服务器）、target（代理服务器地址） \n查看pathRewrite配置文档：https://github.com/chimurai/http-proxy-middleware?tab=readme-ov-file#pathrewrite-objectfunction\n`
+          );
+          return;
+        }
+        done(null, true);
+      },
+    });
+
+    this.multipleProxy = multipleProxy;
+    log.verbose("multipleProxy", multipleProxy);
+  }
+
   async getOrigin() {
     const { origin } = await prompt({
       type: "input",
@@ -224,13 +338,12 @@ class ServerCommand extends Command {
 
   async getPathRewrite() {
     log.info(
-      `pathRewrite的作用是修改代理请求路径，以【空格加冒号加空格】进行分隔，且只能分隔一次\n\n以请求 /api/user 为例：\n\n1. 输入 【/api : ""】 请求路径将被重写为 => /users\n2. 输入 【/api : /test】 请求路径将被重写为 => /test/user\n3. 输入 【/api : /abc/def】 请求路径将被重写为 => /abc/def/user\n`
+      `pathRewrite的作用是修改代理请求路径，填写JSON对象格式 {"key": "value"}，且只能填写一组\n\n以请求 /api/user 为例：\n\n1. 输入 {"/api": ""} 请求路径将被重写为 => /users\n2. 输入 {"/api": "/test"} 请求路径将被重写为 => /test/user\n3. 输入 {"/api": "/abc/def"} 请求路径将被重写为 => /abc/def/user\n`
     );
     const { pathRewrite } = await prompt({
       type: "input",
       name: "pathRewrite",
-      message:
-        "请输入pathRewrite配置：",
+      message: "请输入pathRewrite配置（不需要可按回车键跳过）：",
       default: "", // 默认 ''
       validate(value) {
         const done = this.async();
@@ -240,22 +353,28 @@ class ServerCommand extends Command {
         }
 
         function match(input) {
-          // 使用全局正则表达式匹配所有空格加冒号加空格的实例
-          const matches = input.match(/\s:\s/g);
-          // 检查匹配到的次数是否为1
-          return matches && matches.length === 1;
+          const matches = input.match(
+            /^\s*\{\s*"([^"]+)"\s*:\s*([^,}]+)\s*\}\s*$/
+          );
+          if (matches) {
+            return true;
+          } else {
+            return false;
+          }
         }
         // 匹配空格加冒号加空格
         // 如果已输入则检查是否合法
         if (value && !match(value)) {
-          done("输入的pathRewrite不合法，正确格式：以 [空格加冒号加空格] 隔离前缀和重写路径，且只能分隔一次");
+          done(
+            `输入的pathRewrite不合法，正确格式：{"key": "value"}，key为匹配路径前缀，value为重写路径，且只能写一组`
+          );
           return;
         }
         done(null, true);
       },
     });
 
-    this.pathRewrite = pathRewrite;
+    this.pathRewrite = JSON.stringify(pathRewrite);
     log.verbose("pathRewrite", pathRewrite);
   }
 }
